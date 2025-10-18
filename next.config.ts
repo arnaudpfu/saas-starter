22c2ce6e967dae5420e3877242d856a8ed2ec0be
arnaudpfu <arnaudpfundstein.pro@gmail.com>
2025-10-18 23:11:54 +0200

    update deps and add clean cmd

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  nodeMiddleware: true,
   experimental: {
     ppr: true,
     clientSegmentCache: true,
-    nodeMiddleware: true
   }
 };
 
