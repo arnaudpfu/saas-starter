14363a34984da46718501cec4d95a8cfe0fd9dfa
arnaudpfu <arnaudpfundstein.pro@gmail.com>
2025-10-18 23:20:45 +0200

    update next to canary 16 and replace middleware.ts with proxy.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  nodeMiddleware: true,
   experimental: {
-    ppr: true,
+    cacheComponents: true,
     clientSegmentCache: true,
   }
 };
